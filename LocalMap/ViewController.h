9459f7ccd089cb163a8d33a6faa282eb79b4d31a
guoyuanqiang <guoyuanqiang2008@126.com>
2014-10-21 23:00:00 +0800

    Initial Commit

---
+++ LocalMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalMap
+//
+//  Created by 郭远强 on 14-10-21.
+//  Copyright (c) 2014年 郭远强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocalMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocalMap
+//
+//  Created by 郭远强 on 14-10-21.
+//  Copyright (c) 2014年 郭远强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
